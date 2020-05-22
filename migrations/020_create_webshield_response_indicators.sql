22f3f2182ae5d747f874c94eaa14bd4f9b0ca175
raafaadg@gmail.com <prolord1234$#@!>
2020-05-22 11:28:29 -0300

    Add table

--- migrations/020_create_webshield_response_indicators.sql
+++ migrations/020_create_webshield_response_indicators.sql
@@ -30,3 +30,19 @@ CREATE TABLE eco_webshield_indicator
     index (score)
 );
 
+CREATE TABLE eco_webshield_indicator
+(
+    process_id varchar(512),
+    indicator int(20),
+    label varchar(100),
+    comment varchar(512),
+    score varchar(100),
+
+    created_at datetime DEFAULT CURRENT_TIMESTAMP not null,
+
+    primary key (process_id, indicator, created_at),
+    index (process_id),
+    index (indicator),
+    index (score)
+);
+
